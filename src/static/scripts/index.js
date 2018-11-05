01d9e8cb27ce91c4560e9576826949cf429f2aee
751848178@qq.com <751848178@qq.com>
2018-11-05 23:27:21 +0800

    调整页面样式，新增文案

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,7 +1,15 @@
 $(function() {
     $(".nav-link").click(function (e) {
         // e.preventDefault();
+        // var _this = $(this);
+        // _this.parent().addClass("active").siblings(".active").removeClass("active");
+    });
+    $(".project-tab-item").click(function () {
         var _this = $(this);
-        _this.parent().addClass("active").siblings(".active").removeClass("active");;
+        _this.parent().siblings(".project-tab-content").find(".project-content-item").removeClass("active").eq($(".project-tab-item").index(_this)).addClass("active");
+    });
+    $(".left-btn, .right-btn").click(function () {
+        var _this = $(this);
+        _this.parent().siblings(".project-content-item").removeClass("active").eq(_this.hasClass("left-btn") ? 0 : 1).addClass("active");
     });
 });
